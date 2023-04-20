fdfeddf9be01a42853f875bd8b6db57a1dc19487
Chris Esterbrook <cebrook290@gmail.com>
2023-04-19 21:17:13 -0700

    week 14 state stuff

--- src/components/news-feed.js
+++ src/components/news-feed.js
@@ -5,11 +5,29 @@ let e = React.createElement;
 
 export default class Newsfeed extends React.Component {
     render() {
+        let comments = [
+            {
+                content: 'This is my post',
+                username: 'Tommy',
+                date: '12-2-2020' 
+            },
+            {
+                content: 'This is my post',
+                username: 'Sammy',
+                date: '12-31-2020' 
+            },
+            {
+                content: 'This is my amazing post',
+                username: 'Carolyn',
+                date: '1-2-2021' 
+            }
+        ]; 
         return (
             <div className="container">
+                <Post {...{comments: comments, content: 'This is my post Content!!'}} />
+                <Post {...{content: 'This is ANOTHER post!!! OMG'}} />
                 <Post/>
-                <Post/>
-                <Post/>
+                
             </div>
         )
         
