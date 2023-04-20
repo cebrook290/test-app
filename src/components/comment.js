6c02aecc22c9f8893e92605a163de301cbc36847
Chris Esterbrook <cebrook290@gmail.com>
2023-04-19 21:16:48 -0700

    week 14 state stuff

--- src/components/comment.js
+++ src/components/comment.js
@@ -8,8 +8,12 @@ export default class Comment extends React.Component {
     render() {
         return (
             <div className='card w-75'>
-                <div className='card-header bg-warning text-white'>Username and Time</div>
-                <div className="card-body">Comment Content</div>
+                <div className='card-header bg-info text-white'>
+                    {this.props.username} {this.props.date}
+                    </div>
+                <div className="card-body">
+                    {this.props.content}
+                    </div>
                 <div className="card-footer">
                     <LikeButton/> <ReplyButton/>
                 </div>
